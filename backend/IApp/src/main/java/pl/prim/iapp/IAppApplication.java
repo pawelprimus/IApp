037f4d370732411f6a12104ed398f0ad6dd1259e
Pawel P <pawelprimus@gmail.com>
2024-03-02 11:54:56 +0100

    init backend spring boot project

---
+++ backend/IApp/src/main/java/pl/prim/iapp/IAppApplication.java
@@ -0,0 +1,13 @@
+package pl.prim.iapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IAppApplication.class, args);
+    }
+
+}
---
+++ backend/IApp/src/test/java/pl/prim/iapp/IAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.prim.iapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
