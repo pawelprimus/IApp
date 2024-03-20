3e17e0c493623a431539b85886d59edfd70ad9fd
Pawel P <pawelprimus@gmail.com>
2024-03-20 19:40:18 +0100

    Added editor config

--- backend/IApp/src/main/java/pl/prim/iapp/user/UserController.java
+++ backend/IApp/src/main/java/pl/prim/iapp/user/UserController.java
@@ -8,44 +8,44 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    UserService userService;
+	@Autowired
+	UserService userService;
 
-    @PostMapping("/create")
-    ResponseEntity<?> create(NewUserDto newUser) {
-        User user = userService.createUser(newUser);
-        return ResponseEntity.ok(user);
-    }
+	@PostMapping("/create")
+	ResponseEntity<?> create(NewUserDto newUser) {
+		User user = userService.createUser(newUser);
+		return ResponseEntity.ok(user);
+	}
 
-    //@ApiOperation(value = "Hello world value",
-    //            notes = "Hello world notes")
-    @GetMapping(value = "/getByUsername", produces = "application/json")
-    public ResponseEntity<?> get(String userName) {
-        UserDto user = userService.findByUserName(userName);
-        return ResponseEntity.ok(user);
-    }
+	//@ApiOperation(value = "Hello world value",
+	//            notes = "Hello world notes")
+	@GetMapping(value = "/getByUsername", produces = "application/json")
+	public ResponseEntity<?> get(String userName) {
+		UserDto user = userService.findByUserName(userName);
+		return ResponseEntity.ok(user);
+	}
 
-    @GetMapping(value = "/getAll", produces = "application/json")
-    public ResponseEntity<?> getAll() {
-        return ResponseEntity.ok(userService.findAll());
-    }
+	@GetMapping(value = "/getAll", produces = "application/json")
+	public ResponseEntity<?> getAll() {
+		return ResponseEntity.ok(userService.findAll());
+	}
 
-    @GetMapping(value = "/getById", produces = "application/json")
-    public ResponseEntity<?> getById(Long id) {
-        return ResponseEntity.ok(userService.findById(id));
-    }
+	@GetMapping(value = "/getById", produces = "application/json")
+	public ResponseEntity<?> getById(Long id) {
+		return ResponseEntity.ok(userService.findById(id));
+	}
 
 
-    @DeleteMapping(value = "/delete", produces = "application/json")
-    public ResponseEntity<?> delete(Long id) {
-        userService.deleteById(id);
-        return ResponseEntity.ok("User deleted");
-    }
+	@DeleteMapping(value = "/delete", produces = "application/json")
+	public ResponseEntity<?> delete(Long id) {
+		userService.deleteById(id);
+		return ResponseEntity.ok("User deleted");
+	}
 
-    @PutMapping(value = "/update", produces = "application/json")
-    public ResponseEntity<?> update(Long id, UpdateUserDto updateUserDto) {
-        User user = userService.updateUser(id, updateUserDto);
-        return ResponseEntity.ok(user);
-    }
+	@PutMapping(value = "/update", produces = "application/json")
+	public ResponseEntity<?> update(Long id, UpdateUserDto updateUserDto) {
+		User user = userService.updateUser(id, updateUserDto);
+		return ResponseEntity.ok(user);
+	}
 
 }
